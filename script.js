1df5333bcc43679a4a599f0675e53abf87247937
Anass el ouardy <anaselouardy5@gmail.com>
2026-03-03 16:04:25 +0000

    Create index.html

---
+++ script.js
@@ -0,0 +1,33 @@
+const text = [
+"IT Infrastructure Technician",
+"Network Specialist",
+"MikroTik Engineer",
+"IT Support Expert"
+];
+
+let i=0;
+let j=0;
+let current="";
+let isDeleting=false;
+
+function type(){
+current=text[i];
+
+document.querySelector(".typing").textContent=
+current.substring(0,j);
+
+if(!isDeleting && j++ === current.length){
+isDeleting=true;
+setTimeout(type,1000);
+return;
+}
+
+if(isDeleting && j-- === 0){
+isDeleting=false;
+i=(i+1)%text.length;
+}
+
+setTimeout(type,isDeleting?50:100);
+}
+
+type();
